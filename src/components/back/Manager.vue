e92866e23981968714630a84219f29241f1fafde
jane wu <janewuwz@gmail.com>
2018-04-04 17:27:11 +0800

    Update Manager.vue

--- src/components/back/Manager.vue
+++ src/components/back/Manager.vue
@@ -1,31 +1,11 @@
 <template>
 
   <section class="articles">
-    <!-- <router-link :to="{name:'editor'}"
-                 tag="button">新增文章
-    </router-link> -->
 
     <input class="title" placeholder="商品名称" v-model="g_name">
     <input class="content" placeholder="商品价格" v-model="g_price">
     <button class="save" @click="save">保存</button>
-<!--       <tr v-for="(article,index) in articles">
-        <router-link :to="{name:'article',query:{id:article._id}}"
-                     tag="td">
-          {{article.title}}
-        </router-link>
-        <td>
-          {{article.date | toDate}}
-        </td>
-        <td>
-          <router-link class="fa fa-pencil-square-o"
-                       :to="{name:'editor',query:{id:article._id}}"
-                       tag="i">
-          </router-link>
-          <i class="fa fa-trash"
-             @click="deleteArticle(article._id)">
-          </i>
-        </td>
-      </tr> -->
+
 
   </section>
 </template>
@@ -67,25 +47,7 @@
         }
       }
     }
-    // computed: {
-    //   title: {
-    //     get(){
-    //       return this.$store.state.article.title
-    //     },
-    //     set(value){
-    //       this.$store.commit('UPDATE_TITLE', value)
-    //     }
-    //   },
-    //   content: {
-    //     get(){
-    //       return this.$store.state.article.content
-    //     },
-    //     set(value){
-    //       this.$store.commit('UPDATE_CONTENT', value)
-    //     }
-    //   }
-      
-    // }
+
   }
 </script>
 
