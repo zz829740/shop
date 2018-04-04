7872d93e7df75948408734a735c6bd88fb5436e5
jane wu <janewuwz@gmail.com>
2018-04-04 17:24:49 +0800

    Update Start.vue

--- src/components/font/Start.vue
+++ src/components/font/Start.vue
@@ -1,15 +1,6 @@
 <template>
 <div class="register">
       <button @click="showReg">注册</button>
-      <!-- use the modal component, pass in the prop -->
-     <!--  <modal v-if="showModalr" @close="showModalr = false"> -->
-        <!--
-          you can use custom content here to overwrite
-          default content
-        -->
-        <!-- <h3 slot="header">custom header</h3> -->
-      <!-- </modal> -->
-
       <button @click="showLog">登录</button>
       <login v-if="showModall" @close="showModall = false">
       	
